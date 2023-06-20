3f570a70535cce830978fbc878fa58f96368a9fc
zVi7eRz <vi7er.7@gmail.com>
2023-06-20 17:11:01 +0300

    the job is done

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -8,11 +8,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+            AnimalsCage bean = applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
     }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,13 @@
 package app.model;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
-
     private Long nanoTime = System.nanoTime();
-
+    @Bean
     public Long getTime() {
         return nanoTime;
     }
